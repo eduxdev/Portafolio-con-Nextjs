eff439d733021ede008fcf2246652770b2e21ed6
edu <eduardosvpe@outlook.com>
2025-07-04 15:12:00 -0600

    Intro and Work Experience

---
+++ components/Intro.tsx
@@ -0,0 +1,28 @@
+'use client'
+
+import Image from "next/image";
+
+const Intro = () =>{
+    return(
+        <div className="flex gap-4">
+        <div className="flex flex-col gap-4 w-9/12">
+            <h1 className="text-4xl font-bold">Hola!, Soy yo Edu</h1>
+            <p className="text-base text-gray-700">
+                Ingeniero Fullstack con 1 año de experiencia en strartups. Excelente para trabajar diseñanado aplicaciones que resuelvan problemas de un forma agil y sencilla. Mi experiencia esta basada en le desarrollo de aplicaciones backend como de frontend.
+            </p>
+        </div>
+        <div className="w-3/12 flex-1">
+            <Image 
+            src="/imagenes/EduG.png"
+            alt="Edu" 
+            width={100} 
+            height={100}
+            className="rounded-full border border-b-gray-100 h-36 w-36"
+            />
+        </div>
+        </div>
+
+    )
+}
+
+export default Intro;
---
+++ components/WorkExperience.tsx
@@ -0,0 +1,41 @@
+interface WorkExperienceItem {
+  startDate: string;
+  endDate: string;
+  companyName: string;
+  jobTitle: string;
+  description: string[];
+}
+
+const workExperienceData: WorkExperienceItem[] = [
+  {
+    startDate: "Enero 2024",
+    endDate: "Presente",
+    companyName: "Morfos Digital",
+    jobTitle: "FullStack Developer",
+    description: ["Lider de equipo de desarrollo"],
+  },
+];
+
+const WorkExperience = () => {
+  return (
+    <div className="flex flex-col gap-4">
+      <h1 className="text-2xl font-bold">Experiencia</h1>
+
+      <ol className="relative border-s border-gray-200 dark:border-gray-700">
+        {workExperienceData.map((item, index) => (
+          <li key={index} className="mb-10 ms-4">
+            <div className="absolute w-3 h-3 bg-gray-200 rounded-full mt-1.5 -start-1.5 border border-white dark:border-gray-900 dark:bg-gray-700"></div>
+            <time className="mb-1 text-sm font-normal leading-none text-gray-400 dark:text-gray-500">
+              {item.startDate} - {item.endDate}
+            </time>
+            <h3 className="text-lg font-semibold text-gray-900 dark:text-white">
+              {item.companyName} at {item.jobTitle}
+            </h3>
+          </li>
+        ))}
+      </ol>
+    </div>
+  );
+};
+
+export default WorkExperience;
